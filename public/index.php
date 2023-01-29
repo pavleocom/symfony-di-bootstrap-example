9217c576afc3f72183d1f1d26830fc8332b8975c
Pavel <pavel.leonovic@gmail.com>
2023-01-29 13:56:20 +0000

    caching container for better performance

--- public/index.php
+++ public/index.php
@@ -5,15 +5,32 @@ declare(strict_types=1);
 use App\App;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Dumper\PhpDumper;
 use Symfony\Component\DependencyInjection\Loader\PhpFileLoader;
 
 require '../vendor/autoload.php';
 
-$container = new ContainerBuilder();
+$cacheDir = dirname(__DIR__) . '/var/cache';
+$cachedContainerFile = $cacheDir . '/CachedContainer.php';
 
-(new PhpFileLoader($container, new FileLocator(dirname(__DIR__) . '/config')))->load('services.php');
+if (file_exists($cachedContainerFile)) {
+    require_once $cachedContainerFile;
+    $container = new \CachedContainer(); // name of class given by dumper
+} else {
+    if (!file_exists($cacheDir)) {
+        mkdir($cacheDir, recursive: true);
+    }
 
-$container->compile();
+    $container = new ContainerBuilder();
+
+    (new PhpFileLoader($container, new FileLocator(dirname(__DIR__) . '/config')))->load('services.php');
+
+    $container->compile();
+
+    $dumper = new PhpDumper($container);
+
+    file_put_contents($cachedContainerFile, $dumper->dump(['class' => 'CachedContainer']));
+}
 
 $container->get(App::class)->run();
 
